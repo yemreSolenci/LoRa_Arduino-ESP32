26adb434650065c67dc5ba236a66bb19592d1e81
Yunusemre Şölenci <58556846+yemreSolenci@users.noreply.github.com>
2023-12-16 23:49:04 +0300

    added Bylnk sync

--- ESP32-LoRa-receiver/ESP32-LoRa-receiver.ino
+++ ESP32-LoRa-receiver/ESP32-LoRa-receiver.ino
@@ -1,26 +1,100 @@
+/* Fill-in information from Blynk Device Info here */
+#define BLYNK_TEMPLATE_ID           "TMPL6WddJA5Ye"
+#define BLYNK_TEMPLATE_NAME         "Quickstart Device"
+#define BLYNK_AUTH_TOKEN            "vp3SkhKg9DO26s-OZx8_wz6cimDEUran"
+
 #include <LoRa.h>
 #include <SPI.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32_SSL.h>
+
+
+//  Kablosuz ağ bilgileri
+const char* ssid     = "Xiaomi 11T";
+const char* password = "xgt8x56w";
 
 // 433E6 for Asia
 // 866E6 for Europe
 // 915E6 for North America
 #define BAND 433E6
 
-//packet counter
-int counter = 0;
-String LoRaData = "";
+#define btn 4
 
-boolean waterRelay = false;
 
+int counter = 0;  //packet counter
+String LoRaData = "";   //  Gelen veriler için
+String ledOn = "led on";
+String ledOff = "led off";
 
 void setup() {
-  pinMode(22, OUTPUT);
-  digitalWrite(22, LOW);
+  pinMode(22, OUTPUT);  // Durum göstergesi için build-in ledi kullandım
+  digitalWrite(22, LOW);  // Initialize bölümününde led yanar
+  //pinMode(btn, INPUT_PULLUP);
+  
   //initialize Serial Monitor
-  Serial.begin(9600);
-  Serial.println("LoRa Receiver");
+  Serial.begin(9600);  
+  Serial.println("LoRa Sender");
+  Serial.println();
+  Serial.print("[WiFi] Connecting to "); 
+  Serial.print(ssid);
+  
+  //connectWiFi();
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, password);
+  
+  Serial.println("   " + WiFi.localIP());
+  
   startLoRA();
   digitalWrite(22, HIGH);
+}
+
+void connectWiFi() {
+  WiFi.begin(ssid, password);
+
+  int tryDelay = 500;
+  int numberOfTries = 20;
+
+  // Wait for the WiFi event
+  while (true) {
+
+    switch (WiFi.status()) {
+      case WL_NO_SSID_AVAIL:
+        Serial.println("[WiFi] SSID not found");
+        break;
+      case WL_CONNECT_FAILED:
+        Serial.print("[WiFi] Failed - WiFi not connected! Reason: ");
+        return;
+        break;
+      case WL_CONNECTION_LOST:
+        Serial.println("[WiFi] Connection was lost");
+        break;
+      case WL_SCAN_COMPLETED:
+        Serial.println("[WiFi] Scan is completed");
+        break;
+      case WL_DISCONNECTED:
+        Serial.println("[WiFi] WiFi is disconnected");
+        break;
+      case WL_CONNECTED:
+        Serial.println("[WiFi] WiFi is connected!");
+        Serial.print("[WiFi] IP address: ");
+        Serial.println(WiFi.localIP());
+        return;
+      default:
+        Serial.print("[WiFi] WiFi Status: ");
+        Serial.println(WiFi.status());
+        break;
+    }
+    delay(tryDelay);
+
+    if (numberOfTries <= 0) {
+      Serial.print("[WiFi] Failed to connect to WiFi!");
+      // Use disconnect function to force stop trying to connect
+      WiFi.disconnect();
+      return;
+    } else {
+      numberOfTries--;
+    }
+  }
 }
 
 //Initialize LoRa module
@@ -36,6 +110,7 @@ void startLoRA() {
     Serial.println("LoRa init failed. Check your connections.");
     delay(100);
     while (1) {
+      // Initialize LoRa başarısız olduğunda uyarı için led yanıp söner
       digitalWrite(22, LOW);
       delay(300);
       digitalWrite(22, HIGH);
@@ -53,12 +128,32 @@ void startLoRA() {
   }
 
   Serial.println("LoRa Initialization OK!");
-  counter = 1;
+  counter = 0;
   delay(2000);
 }
 
-void loop() {
+//  Drive LED
+void sendPacket(String data){
+  LoRa.beginPacket();   // Send LoRa packet
+  LoRa.print(data);
+  LoRa.endPacket();
+  counter++;
+  Serial.print("Sending packet: "+ data);
+  Serial.println(counter);
+}
 
+//  Bylnk'ten veri geldiğinde çalışır
+BLYNK_WRITE(V0) {   
+  digitalWrite(22, LOW);
+  // integer olarak değer al ve değere göre packet gönder
+  param.asInt() == true ? sendPacket("led on") : sendPacket("led off");
+  digitalWrite(22, HIGH);
+}
+
+void loop() {
+  Blynk.run();
+
+  //  Lora gelen paket varmı kontrol etme
   int packetSize = LoRa.parsePacket();
   if (packetSize)
   {
@@ -66,16 +161,29 @@ void loop() {
     Serial.print("Received packet '");
 
     while (LoRa.available()) {
-      Serial.print((char)LoRa.read());
+      LoRaData += (char)LoRa.read();
     }
     
+    Serial.print(LoRaData);
+
     // print RSSI of packet
     Serial.print(F("' | RSSI: "));
     Serial.println(LoRa.packetRssi());
-    
-    digitalWrite(22, HIGH);
 
+
+    // Send state to Bylnk for sync
+    if (LoRaData.equals(ledOn))
+    {
+      Blynk.virtualWrite(V0, 1);
+      Serial.println("sent Bylnk v0: 1");
+    }   else if (LoRaData.equals(ledOff)) {
+      Blynk.virtualWrite(V0, 0);
+      Serial.println("send Bylnk v0: 0");
+    } else  Serial.println("Hatalı mesaaj");
+
+    LoRaData = "";  //  Gelecek bir sonraki paket için Stringi temizle
     counter++;
-  }
 
+    digitalWrite(22, HIGH);
+  }
 }
--- Uno-LoRa-transmitter/Uno-LoRa-transmitter.ino
+++ Uno-LoRa-transmitter/Uno-LoRa-transmitter.ino
@@ -6,17 +6,27 @@
 // 915E6 for North America
 #define BAND 433E6
 
-//packet counter
+#define led 8
+#define button 7
 
-int counter = 0;
-String packet = "";
+int ledState;
 
-boolean waterRelay = false;
+int counter = 0;  // packet counter
+
+// Bylnk ile gelen veri için
+String LoRaData = "";
+String ledOn = "led on";
+String ledOff = "led off";
+
 
 void setup() {
+  pinMode(led, OUTPUT);
+  pinMode(button, INPUT_PULLUP);
+
   //initialize Serial Monitor
   Serial.begin(9600);
-  Serial.println("LoRa Sender");
+  Serial.println("LoRa Receiver");
+
   startLoRA();
 }
 
@@ -29,31 +39,81 @@ void startLoRA() {
     delay(1000);
   }
   if (counter == 5) {
-    Serial.println("Starting LoRa failed!");
+    Serial.println("LoRa init failed. Check your connections.");
     delay(100);
-    while (1);
+    while (1) {
+      delay(10000);
+    }
   }
-  
+
   Serial.println("LoRa Initialization OK!");
-  counter = 1;
+  counter = 0;
+  delay(2000);
+}
+
+
+//  Drive LED
+void updateLEDState() {
+  if (ledState == true)
+  {
+    digitalWrite(led, HIGH);
+    Serial.println("led on");
+  } else {
+    digitalWrite(led, LOW);
+    Serial.println("led off");
+  }
 }
 
 void loop()
 {
+  if (digitalRead(button) == LOW)  // Fiziksel butona basıldı
+  {
+    ledState = !ledState;
 
-  // print RSSI of packet
-  Serial.print(" | RSSI: ");
-  Serial.println(LoRa.packetRssi());
+    // ledin durumunu değiştirilmesi
+    updateLEDState();
 
-  Serial.print("Sending packet: ");
-  Serial.println(counter);
+    // Bylnk senkronizasyonu için durum değişikliğinin gönderilmesi
+    LoRa.beginPacket();   // Send LoRa packet
+    LoRa.print("led ");
+    ledState ? LoRa.print("on") : LoRa.print("off");
+    LoRa.endPacket();
+    counter++;
+    Serial.print("Sending packet: ");
+    Serial.println(counter);
 
-  LoRa.beginPacket();   //Send LoRa packet to receiver
-  LoRa.print("hello ");
-  LoRa.print(counter);
-  LoRa.endPacket();
+    delay(500);
+  }
 
-  counter++;
+  //  Lora gelen paket varmı kontrol etme
+  int packetSize = LoRa.parsePacket();
+  if (packetSize)
+  {
+    while (LoRa.available()) {
+      LoRaData += (char)LoRa.read();
+    }
 
-  delay(5000);
+    //  Alınan paketi yaz
+    Serial.print("Received packet '");
+    Serial.print(LoRaData);
+
+    // print RSSI of packet
+    Serial.print(F("' | RSSI: "));
+    Serial.println(LoRa.packetRssi());
+
+    // Toggle led state
+    if (LoRaData.equals(ledOn)) {
+      ledState = 1;
+      updateLEDState();
+      //Blynk.virtualWrite(V0, rcvBtnState);
+      //Serial.println("ver v0: "+ String(rcvBtnState));
+    } else if (LoRaData.equals(ledOff)) {
+      ledState = 0;
+      updateLEDState();
+    } else  Serial.println("Hatalı mesaaj");
+
+    LoRaData = "";  //  Gelecek bir sonraki paket için Stringi temizle
+
+    counter++;
+  }
 }
